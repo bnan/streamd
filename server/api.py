31a3e85eae0d9d8f93f70942962ab2b7265a2cc8
Luís Silva <lfssilva@ua.pt>
2019-04-05 23:54:25 +0100

    basic comments implementation

--- server/api.py
+++ server/api.py
@@ -1,26 +1,32 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+from git import Repo
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
-@app.route('/api/v1/routes/<lat>/<lng>/', methods=['GET'])
-def routes(lat, lng):
-    try:
-        results = [
-            { 'lat': -8.6721462, 'lng': 41.165162 },
-            { 'lat': -8.684325, 'lng': 41.1735278 },
-            { 'lat': -8.6882191, 'lng': 41.1734336 },
-            { 'lat': -8.6131861, 'lng': 41.1718621 }
-        ]
+@app.route('/api/v1/comments/<remote_id>', methods=['POST'])
+def routes(remote_id):
+    repo_dir = f'~/streamd/{remote_id}'
 
-        return jsonify(**{'error': False, 'message': f'Successfully computed routes for ({lat}, {lng})', 'results': results })
-    except Exception as e:
-        raise e
-        return jsonify(**{ 'error': True, 'message': str(e), 'results': [] })
+    try:
+        username = request.form['username']
+        text     = request.form['text']
+    except KeyError:
+        # You must provide those form parameters
+        return jsonify(**{ 'error': True, 'message': 'Mal-formed message' })
+
+
+    repo = Repo(repo_dir)
+    if repo.bare:
+        # There is no such repo you silly boy
+        return jsonify(**{ 'error': True, 'message': 'Could not find repo' })
+
+
+    with open(
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1337)
+    app.run(host='0.0.0.0', port=1337, debug=True)
