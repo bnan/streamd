fdd5a5b7f98d24b7127044b3467638f359be5640
Ricardo Jesus <rj.bcjesus@gmail.com>
2019-04-06 01:47:06 +0100

    Add draft of repo creation (on streamer and server)

--- server/api.py
+++ server/api.py
@@ -1,9 +1,23 @@
+import os
+import random
+import string
+import subprocess
+import tempfile
+
+from pathlib import Path
+
+import flask
+import git
+from git import Repo
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+REPO_BASE = os.path.join(Path.home(), 'streamd')
+REPO_ID_LENGTH = 6  # use 6 characters for repo ids
 
 
 @app.route('/api/v1/routes/<lat>/<lng>/', methods=['GET'])
@@ -20,6 +34,38 @@ def routes(lat, lng):
     except Exception as e:
         raise e
         return jsonify(**{ 'error': True, 'message': str(e), 'results': [] })
+
+
+@app.route('/new/repo', methods=['POST'])
+def new_repository():
+    bundle_f = flask.request.files.get('bundle')
+
+    # generate a random id for this repo
+    while True:
+        repo_id = ''.join(random.choices(string.ascii_letters + string.digits,
+                                         k=REPO_ID_LENGTH))
+
+        repo_path = os.path.join(REPO_BASE, repo_id)
+
+        try:
+            repo = Repo(os.path.join(repo_path))
+        except git.exc.NoSuchPathError:
+            break
+
+    if bundle_f is not None:
+        h,bundle = tempfile.mkstemp(suffix='.bundle')
+        bundle_f.save(bundle)
+
+        subprocess.run(['git', 'clone', '-b', 'master', bundle, repo_id],
+                       cwd=REPO_BASE)
+
+        os.remove(bundle)
+    else:
+        Repo.init(os.path.join(repo_path))
+
+    open(os.path.join(repo_path, '.git', 'git-daemon-export-ok'), 'w').close()
+
+    return repo_id
 
 
 if __name__ == '__main__':
