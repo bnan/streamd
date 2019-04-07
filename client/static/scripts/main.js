2c9b1f339f915cc0c6ad728cb8889f3f6bb9eb20
Luís Silva <lfssilva@ua.pt>
2019-04-07 15:35:41 +0100

    end chat

--- client/static/scripts/main.js
+++ client/static/scripts/main.js
@@ -6,11 +6,32 @@ async function updateChat(chatEl, repo_id){
     try {
         let response = await fetch(`${API_URL}/comments/${repo_id}`)
         let json = await response.json()
-        console.log("PILLAAAAAAAAAAAA")
         console.log(json)
 
-        for( elem in json ){
+        for(let elem of Object.keys(json)  ) {
+
             console.log(elem)
+            for(let message of Object.keys(json[elem])  ) {
+                console.log(json[elem][message])
+
+                var li = document.createElement("li");
+                var input = document.createElement("input");
+                var h5 = document.createElement("h5");
+                var p = document.createElement("p");
+
+                h5.innerHTML = json[elem][message]["username"] +" @ ";
+                p.innerHTML = json[elem][message]["text"];
+                input.setAttribute("type","button");
+                input.setAttribute("id","commit-text");
+                input.setAttribute("onClick","changeCommit(this.value)");
+                input.setAttribute("value",repo_id);
+
+                h5.appendChild(input);
+                li.appendChild(h5)
+                li.appendChild(p)
+
+                chatEl.appendChild(li);
+            }
         }
 
     } catch (error) {
@@ -43,43 +64,43 @@ async function send_comment(remote_id, thread, username, comment){
 }
 
 async function commits(repoDir) {
-  try {
-    let response = await fetch(`${STREAM_URL}/commits/${repoDir}`)
-    let json = await response.json()
-    return json
-  } catch (error) {
-    console.error(error)
-  }
+    try {
+        let response = await fetch(`${STREAM_URL}/commits/${repoDir}`)
+        let json = await response.json()
+        return json
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 async function commit(repoDir, commit) {
-  try {
-    let response = await fetch(`${STREAM_URL}/commit/${repoDir}/${commit}`)
-    console.log(response)
-  } catch (error) {
-    console.error(error)
-  }
+    try {
+        let response = await fetch(`${STREAM_URL}/commit/${repoDir}/${commit}`)
+        console.log(response)
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 async function list(repoDir) {
-  try {
-    let response = await fetch(`${STREAM_URL}/files/${repoDir}`)
-    let json = await response.json()
-    return json
-  } catch (error) {
-    console.error(error)
-  }
+    try {
+        let response = await fetch(`${STREAM_URL}/files/${repoDir}`)
+        let json = await response.json()
+        return json
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 async function load(filename) {
-  try {
-    let response = await fetch(`${WATCHD_URL}/${filename}`)
-    let file = await response.text()
-    document.querySelector('#source pre code').innerHTML = file
-    return file
-  } catch (error) {
-    console.error(error)
-  }
+    try {
+        let response = await fetch(`${WATCHD_URL}/${filename}`)
+        let file = await response.text()
+        document.querySelector('#source pre code').innerHTML = file
+        return file
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 let syncing = true
@@ -88,36 +109,36 @@ let repoDir = ''
 
 
 async function main() {
-  let pieces = window.location.href.split('/')
-  repoDir = pieces[pieces.length-1]
-  //const extension = 'python'
+    let pieces = window.location.href.split('/')
+    repoDir = pieces[pieces.length-1]
+    //const extension = 'python'
 
-  const elCode = document.querySelector('#source pre code')
-  elCode.onselectstart = function() {
-    syncing = false
-  }
-  elCode.onmouseup = function() {
-    if (!syncing) {
-      document.execCommand('copy')
-      syncing = true
+    const elCode = document.querySelector('#source pre code')
+    elCode.onselectstart = function() {
+        syncing = false
     }
-  }
-
-  const elTree = document.querySelector('#maindir')
-
-  let file = await load(`${repoDir}/STREAMD.md`)
-
-  const elChat = document.querySelector('#ul-chat')
-
-  setInterval(async () => {
-    if (syncing) {
-      updateChat(elChat, repoId)
-
-      let filenames = await list(repoDir)
-      commitsList = await commits(repoDir)
-      traverse(filenames, elTree)
+    elCode.onmouseup = function() {
+        if (!syncing) {
+            document.execCommand('copy')
+            syncing = true
+        }
     }
-  }, 1000)
+
+    const elTree = document.querySelector('#maindir')
+
+    let file = await load(`${repoDir}/STREAMD.md`)
+
+    const elChat = document.querySelector('#ul-chat')
+
+    setInterval(async () => {
+        if (syncing) {
+            updateChat(elChat, repoDir)
+
+            let filenames = await list(repoDir)
+            commitsList = await commits(repoDir)
+            traverse(filenames, elTree)
+        }
+    }, 1000)
 }
 
 main()
